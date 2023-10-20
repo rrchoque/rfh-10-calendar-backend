17847fd23f458a4bd6b4038c3652b709689139be
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-10-20 16:21:56 -0400

    Se instaló express-validator y se configuró las validaciones de login y new

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,28 @@
 */
 const { Router } = require("express");
 const { crearUsuario, loginUsuario, revalidarToken } = require("../controllers/auth");
+const { check } = require("express-validator");
 
 const router = Router();
 
-router.post( '/new', crearUsuario );
+router.post(
+    '/new', 
+    [ // middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    ],
+    crearUsuario 
+);
 
-router.post( '/', loginUsuario);
+router.post(
+    '/',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    ],
+    loginUsuario 
+);
 
 router.get( '/renew', revalidarToken);
 
