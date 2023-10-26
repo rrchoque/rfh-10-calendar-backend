f1e605506b0052a24872481685287f83ab66fb10
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-10-26 00:03:02 -0400

    Se creó la funcionalidad para revalidar los tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -91,11 +91,16 @@ const loginUsuario = async (req, res = response) => {
     }
 }
 
+const revalidarToken = async (req, res = response ) => {
 
-const revalidarToken = (req, res = response) => {
+    const { uid, name } = req;
+
+    // Generar JWT
+    const token = await generarJWT( uid, name );
+
     res.json({
         ok: true,
-        msg: 'renew'
+        token
     })
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const { Router } = require("express");
 const { crearUsuario, loginUsuario, revalidarToken } = require("../controllers/auth");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
@@ -30,6 +31,6 @@ router.post(
     loginUsuario 
 );
 
-router.get( '/renew', revalidarToken);
+router.get( '/renew', validarJWT, revalidarToken);
 
 module.exports = router;
