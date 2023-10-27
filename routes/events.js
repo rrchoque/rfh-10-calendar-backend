200ab301a8203d23259e260b2933b24e9d8bc29a
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-10-26 23:22:16 -0400

    Se agregó validación de JWT

--- routes/events.js
+++ routes/events.js
@@ -4,9 +4,12 @@
 */
 const { Router } = require('express');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
+// Todas tienes que pasar por la validación del JWT
+router.use( validarJWT );
 
 // Obtener eventos 
 router.get('/', getEventos );
